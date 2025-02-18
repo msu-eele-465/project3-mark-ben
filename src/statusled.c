99045c96644e539fe1e64bf47cebbb0d93d98700
Mark-H-W <kk7byr@wildmile.com>
2025-02-18 10:36:31 -0700

    add status led code outline

---
+++ src/statusled.c
@@ -0,0 +1,14 @@
+#include <msp430.h>
+#include <stdbool.h>
+
+
+// This function will update the current status of the led
+void updateled(int current_status) {
+    switch (current_status) {
+        case 0:
+        
+        case 1:
+
+        case 2:
+    }
+}
