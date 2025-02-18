1ae3ea3e04909f3c95c2b69f526b589d50fc129b
Mark-H-W <kk7byr@wildmile.com>
2025-02-18 11:15:03 -0700

    statusled updates

--- src/statusled.c
+++ src/statusled.c
@@ -1,12 +1,15 @@
 #include <msp430.h>
 #include <stdbool.h>
 
+//3.2 - Red 
+//3.3 - Green
+//3.7 - Blue
 
 // This function will update the current status of the led
-void updateled(int current_status) {
-    switch (current_status) {
+void updateled(void) {
+    switch (state_variable) {
         case 0:
-        
+            
         case 1:
 
         case 2:
