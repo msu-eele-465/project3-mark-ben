c2b81cef8872dbebafe5227877df378da43484ef
Mark-H-W <kk7byr@wildmile.com>
2025-02-18 11:23:12 -0700

    statusled code

--- src/statusled.c
+++ src/statusled.c
@@ -9,9 +9,50 @@
 void updateled(void) {
     switch (state_variable) {
         case 0:
-            
+            red_count = 0xc4;
+            green_count = 0x3e;
+            blue_count = 0x1d;
+            break;
         case 1:
-
+            red_count = 0x1d;
+            green_count = 0xa2;
+            blue_count = 0xc4;
+            break;
         case 2:
+            red_count = 0xc4;
+            green_count = 0x92;
+            blue_count = 0x1d;
+            break;
     }
+}
+
+#pragma vector=TIMER_B1_VECTOR
+__interrupt void Timer_B0_ISR(void) {
+    status_led_count++;
+
+    if (status_led_count > 255) {
+        status_led_count = 0;
+    }
+
+    // RED PWM
+    if (status_led_count < red_count) {
+        P3OUT |= BIT2;
+    } else {
+        P3OUT &= ~BIT2;
+    }
+
+    // GREEN PWM
+    if (status_led_count < green_count) {
+        P3OUT |= BIT3;
+    } else {
+        P3OUT &= ~BIT3;
+    }
+
+    // BLUE PWM
+    if (status_led_count < blue_count) {
+        P3OUT |= BIT7;
+    } else {
+        P3OUT &= ~BIT7;
+    }
+
 }
